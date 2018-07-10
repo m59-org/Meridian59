9673bafb2df138ccb99205ee44f878058d3f1a6d
GarAdminTools <meridian59gar4@gmail.com>
2018-07-09 21:06:36 -0400

    Stat change preliminary work

--- blakserv/sprocket.c
+++ blakserv/sprocket.c
@@ -79,7 +79,7 @@ client_def_table_type client_def_table[] =
 	{ BP_CHANGED_STATS,               { {1, TAG_INT}, {1, TAG_INT}, {1, TAG_INT}, {1, TAG_INT},
                                        {1, TAG_INT}, {1, TAG_INT},{1, TAG_INT}, {1, TAG_INT},
                                        {1, TAG_INT}, {1, TAG_INT}, {1, TAG_INT}, {1, TAG_INT},
-                                       {1, TAG_INT}, {0, DONE_PARM} } },
+                                       {1, TAG_INT}, {1, TAG_INT}, {1, TAG_INT}, {0, DONE_PARM} } },
 	
 };
 int num_client_msgs = sizeof(client_def_table)/sizeof(client_def_table_type);
